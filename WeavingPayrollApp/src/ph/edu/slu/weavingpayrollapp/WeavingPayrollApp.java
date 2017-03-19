cbd3b64ab788650037a02b6df0f9e788464a6f00
Christopher Espiritu <christopheredrian@gmail.com>
2017-03-20 00:57:26 +0800

    Add initial project and user interface

---
+++ WeavingPayrollApp/src/ph/edu/slu/weavingpayrollapp/WeavingPayrollApp.java
@@ -0,0 +1,16 @@
+package ph.edu.slu.weavingpayrollapp;
+
+/**
+ *
+ * @author chris
+ */
+public class WeavingPayrollApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
